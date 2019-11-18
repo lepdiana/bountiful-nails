0f8a667bc4820206cf6f92a520f64c1b141279e6
Diana Le <lepdiana01@gmail.com>
2019-11-18 15:40:07 -0700

    added in services

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return(
+        <div>
+            <h1>Our Vision</h1>
+            <p>Our focus is to deliver a finished product matched specific to each guest's own uniqueness.</p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/ContactUs.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ContactUs = () => {
+    return(
+        <div>
+            <h2>325 South 500 West</h2>
+            <h2>Bountiful, UT 84010</h2>
+            <h2>Phone: 801-295-5368</h2>
+
+            <h2>Hours:</h2>
+            <p>Monday - Saturday: 9:30am - 7:00pm</p>
+            <p>Sunday: CLOSED</p>
+            <p>Hours are subject to change during holidays.</p>
+
+            <p>Walk-in welcome.</p>
+            <p>Give us a call to set up an appointment.</p>
+        </div>
+    )
+}
+
+export default ContactUs
