ee960cfedabd7816943accbc55d2e97a1f50f851
Diana Le <lepdiana01@gmail.com>
2019-11-20 10:49:46 -0700

    added in the home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+import Home from './Components/Home.js'
+
 
 const App = () => {
   return (
     <div className="App">
-      Yay! this works
+      <Switch>
+        <Route exact path='/' component={Home}/>
+      </Switch>
     </div>
   );
 }
---
+++ client/src/Components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Carousel } from "react-responsive-carousel";
+import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const Home = () => {
+    return(
+        <div>
+            <Carousel showThumbs={false} infiniteLoop={true}>
+                <div style={{ height: "100vh", color: "#fff" }}><h1>Bountiful Nails</h1></div>
+                <div style={{ height: "100vh", color: "#fff" }}>this is slide 2</div>
+                <div style={{ height: "100vh", color: "#fff" }}>this is slide 3</div>
+            </Carousel>
+        </div>
+    )
+}
+
+export default Home
