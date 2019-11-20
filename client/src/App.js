c0ce6e939e60da8e0d397bc4c138fa49113d4313
Diana Le <lepdiana01@gmail.com>
2019-11-20 16:07:21 -0700

    added in the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import Home from './Components/Home.js'
+import Navbar from './Components/Navbar.js'
+import About from './Components/About.js'
 
 
 const App = () => {
   return (
     <div className="App">
+      <Navbar />
       <Switch>
         <Route exact path='/' component={Home}/>
+        <Route path='/about' component={About}/>
       </Switch>
     </div>
   );
