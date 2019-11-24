ff5e99bfc638ea2645e1ce3b625a21edd9dee66a
Diana Le <lepdiana01@gmail.com>
2019-11-24 15:27:00 -0700

    added in the text for the about us section

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -3,7 +3,8 @@ import React from 'react'
 const About = () => {
     return(
         <div>
-            this is the about page
+            <h1>Our Vision</h1>
+            <p>Bountiful Nails offer a variety of services that are accommodating to your wants and needs.Our focus is to deliever a finished product matched specific to each guest's own uniqueness.</p>
         </div>
     )
 }
