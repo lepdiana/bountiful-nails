1adf8d538738eb5f1bf6ab049c06444ae6e2d698
Diana Le <lepdiana01@gmail.com>
2019-12-16 19:26:12 -0700

    added in a scroll paralax for the about section

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -1,10 +1,34 @@
 import React from 'react'
 
+import '../Styles/about.css'
+
 const About = () => {
+
+    function scrollBanner() {
+        var scrollPos;
+        var headerText = document.querySelector('.header-paralax h1');
+        scrollPos = window.scrollY;
+      
+        if (scrollPos <= 600) {
+          headerText.style.transform = "translateY(" + (-scrollPos / 3) + "px" + ")";
+          headerText.style.opacity = 1 - (scrollPos / 600);
+        }
+      }
+      window.addEventListener('scroll', scrollBanner);
+
+      
     return(
         <div>
-            <h1>Our Vision</h1>
-            <p>Bountiful Nails offer a variety of services that are accommodating to your wants and needs. We have over 500+ of shellac gel polish, 100+ regular nail polish, 50+ nail tips, 20+ dipping powder, and many more. Our focus is to deliver a finished product matched specific to each guest's own uniqueness.</p>
+            <div className="invisibileBox"></div>
+            <header className='header-paralax'>
+                <h1>Our Vision</h1>
+            </header>
+            <main>
+                <div className='aboutContainer'>
+                    <p>Bountiful Nails offer a variety of services that are accommodating to your wants and needs. We have over 500+ of shellac gel polish, 100+ regular nail polish, 50+ nail tips, 20+ dipping powder, and many more. Our focus is to deliver a finished product matched specific to each guest's own uniqueness.</p>
+                </div>
+            </main>
+
         </div>
     )
 }
--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import { Carousel } from "react-responsive-carousel";
 import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
+import '../Styles/home.css'
 
 const Home = () => {
     return(
-        <div>
+        <div className='homeContainer'>
+            <div className="invisibileBox"></div>
             <Carousel showThumbs={false} infiniteLoop={true}>
-                <div style={{ height: "100%", color: "#fff" }}>
-                    <img src="https://i.postimg.cc/5N0d8g9C/Momma-Ho-s-2.jpg" style={{ height: "100vh", width: "100vw"}}/>
+                <div style={{ height: "91vh", color: "#fff" }}>
+                    <img src="https://i.postimg.cc/5N0d8g9C/Momma-Ho-s-2.jpg" style={{ height: "91vh", width: "100vw"}}/>
                 </div>
-                <div style={{ height: "100%", color: "#fff" }}>
-                    <img src="https://i.postimg.cc/ryg2d3TT/Momma-Ho-s-15.jpg" style={{ height: "100vh", width: "100vw"}}/>
+                <div style={{ height: "91vh", color: "#fff" }}>
+                    <img src="https://i.postimg.cc/ryg2d3TT/Momma-Ho-s-15.jpg" style={{ height: "91vh", width: "100vw"}}/>
                 </div>
-                <div style={{ height: "100%", color: "#fff" }}>
-                    <img src="https://i.postimg.cc/c4bqh365/Momma-Ho-s-6.jpg" style={{ height: "100vh", width: "100vw"}}/>
+                <div style={{ height: "91vh", color: "#fff" }}>
+                    <img src="https://i.postimg.cc/c4bqh365/Momma-Ho-s-6.jpg" style={{ height: "91vh", width: "100vw"}}/>
                 </div>
-                <div style={{ height: "100%", color: "#fff" }}>
-                    <img src="https://i.postimg.cc/9MhHTjzm/Momma-Ho-s-22.jpg" style={{ height: "100vh", width: "100vw"}}/>
+                <div style={{ height: "91vh", color: "#fff" }}>
+                    <img src="https://i.postimg.cc/9MhHTjzm/Momma-Ho-s-22.jpg" style={{ height: "91vh", width: "100vw"}}/>
                 </div>
             </Carousel>
         </div>
